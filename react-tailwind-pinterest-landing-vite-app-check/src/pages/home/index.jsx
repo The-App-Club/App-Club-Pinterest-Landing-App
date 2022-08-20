d52aaefe274691645797343d44138276a33a3e2a
Higashi-Kota <higashi.kota@gri.jp>
2022-08-21 00:21:17 +0900

    fix viewport height

--- react-tailwind-pinterest-landing-vite-app-check/src/pages/home/index.jsx
+++ react-tailwind-pinterest-landing-vite-app-check/src/pages/home/index.jsx
@@ -26,7 +26,22 @@ import {Tomorrow} from '../../components/Tomorrow';
 import {CreateAccount} from '../../components/CreateAccount';
 import {Form} from '../../components/Form';
 
+import {useDebouncedCallback} from 'use-debounce';
+import {useEffect} from 'react';
+
 const HomePage = ({pageName, notifier}) => {
+  const handleResize = useDebouncedCallback((e) => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, 600);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <Layout pageName={pageName} notifier={notifier}>
       <section
@@ -71,7 +86,8 @@ const HomePage = ({pageName, notifier}) => {
               height: 100%;
               .swiper-wrapper {
                 width: 100%;
-                height: calc(100vh - 3rem);
+                min-height: 100vh; /* Fallback */
+                min-height: calc(var(--vh, 1vh) * 100);
                 .swiper-slide {
                   padding: 0 9rem;
                   @media (max-width: 768px) {
@@ -92,12 +108,12 @@ const HomePage = ({pageName, notifier}) => {
           <SwiperSlide>
             <Tomorrow />
           </SwiperSlide>
-          <SwiperSlide>
+          {/* <SwiperSlide>
             <CreateAccount />
           </SwiperSlide>
           <SwiperSlide>
             <Footer />
-          </SwiperSlide>
+          </SwiperSlide> */}
         </Swiper>
       </section>
     </Layout>
